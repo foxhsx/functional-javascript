8048ece742933579d980debd3d98acda5572385b
foxhsx <932163475@qq.com>
2021-08-10 23:23:32 +0800

    add array functional

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -3,7 +3,13 @@ import {
   forEachObject, 
   unless,
   times,
-  every
+  every,
+  some,
+  tap,
+  unary,
+  once,
+  memoized,
+  arrayUtils
 } from "../lib/es6-functional";
 
 // var array = [1, 2, 3]
@@ -32,5 +38,32 @@ import {
 //   })
 // })
 
-console.log(every([NaN, NaN, NaN], isNaN))
-console.log(every([NaN, NaN, 4], isNaN))
+// console.log(some([NaN, NaN, 4], isNaN))
+// console.log(some([2, 3, 4], isNaN))
+
+// const arr = ['1', '2', '3'].map(unary(parseInt))
+// console.log(arr, 'arr');
+
+// var doPayment = once(() => {
+//   console.log('Payment is done');
+// })
+
+// doPayment();
+// doPayment();
+
+// let fastFactorial = memoized((n) => {
+//   if (n === 0) {
+//     return 1;
+//   }
+
+//   // 递归计算阶乘
+//   return n * fastFactorial(n - 1)
+// })
+
+// console.log(fastFactorial(5))
+
+// const arr = arrayUtils.map([1,2,3], (x) => x * x)
+// console.log(arr);
+
+const arr = arrayUtils.concatAll([[1,2,3], [2,3,4]])
+console.log(arr);
